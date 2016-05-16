cb155ba1be627cff8dc9d6b741c17484816c16f6
Chris Bosco <cbosco@ondeck.com>
2016-05-16 18:56:03 -0400

    Remove options and use convention of config.json file inside inputTree root

--- example/Brocfile.js
+++ example/Brocfile.js
@@ -3,8 +3,6 @@ var mergeTrees = require('broccoli-merge-trees');
 var wintersmith = require('broccoli-wintersmith');
 var wintersmithPublicOutput = 'basic-wintersmith-site';
 
-wintersmithPublicOutput = wintersmith(wintersmithPublicOutput, {
-    config: require('path').resolve('./basic-wintersmith-site/config.json')
-});
+wintersmithPublicOutput = wintersmith(wintersmithPublicOutput);
 
 module.exports = mergeTrees([wintersmithPublicOutput]);
